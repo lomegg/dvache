7343c279e10a45f38fb8d25438f688387dc36fa4
Lomegg <unrelated@ya.ru>
2013-07-19 08:58:03 -0700

    Structured files

--- dvach.rb
+++ dvach.rb
@@ -7,10 +7,10 @@ Bundler.require
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
-Mongoid.load! './mongoid.yml', :development          # рубимайн требует уточнить сдк и вообще этого гема нет
+Mongoid.load! './mongoid.yml', :development
 
 class Post
-  include Mongoid::Document                         #что вообще монгоид делает, пойти почитать
+  include Mongoid::Document
   include Mongoid::Timestamps
 
   field :text, type: String
@@ -29,19 +29,21 @@ end
 Post.all.empty_posts.delete_all
 
 get '/' do
-  @posts = Post.all                              #т.е. это не рельсы? они же сами должны это уметь
+  @posts = Post.all
+
 
   slim :index
 end
 
 post '/' do
-  @text = RedCloth.new(params['text']).to_html         #RedCloth?
+
+  @text = RedCloth.new(params['text']).to_html
 
   if (failname = params['failname']) && failname[:type] == 'image/jpeg'
     src = failname[:tempfile].path
     dst = "#{Time.now.to_i}.jpg"
     FileUtils.cp(src, "#{settings.public_folder}/#{dst}")
-  end               #аттачмент
+  end
 
   @post = Post.create(text: @text, image: dst)
 
@@ -53,72 +55,18 @@ get '/post/:num' do |n|
   slim :post, locals: {post: post}
 end
 
+#get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params[:name] is 'foo' or 'bar'
+#  "Hello #{params[:name]}!"
+#end
+
+
+
+
+
+
 __END__
-@@ layout
-html
-  head
-    link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet"
-    title Недвач совсем
-
-    css:
-      @import url(http://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700&subset=latin,cyrillic);
-      body, p, h1, h2, h3, h4, h5, h6 {
-        font-family: 'PT Sans Narrow', 'Helvetica Neue', 'Arial', sans-serif;
-      }
-
-      .thumbnail.post img {
-        -ms-transform:rotate(2deg); /* IE 9 */
-        -moz-transform:rotate(2deg); /* Firefox */
-        -webkit-transform:rotate(2deg); /* Safari and Chrome */
-        -o-transform:rotate(2deg); /* Opera */
-        transform:rotate(2deg);
-      }
-      
-      .thumbnail.post img {
-        box-shadow:2px 2px 3px #000;
-      }
-    
-  body
-    .container
-      .row
-        .span12
-          h1
-            a href='/'
-              ' Недвач
-              small Всего постов: #{Post.count}
-
-        .span5
-          == erb :form
-        .span7
-          == yield
-          h6
-            ' using
-            a href="http://en.wikipedia.org/wiki/Textile_%28markup_language%29"
-             | textile
-
-@@ index
-- @posts.each do |post|
-  == slim :post, locals: {post: post}
-
-@@ form
-<form action="" method=post enctype="multipart/form-data" class="well form-inline">
-  <input type=file accept="image/jpeg" name=failname class="span6"><br/><br/>
-  <textarea name=text class="span4" placeholder="Выразите свои мысли, чувства, вот это все. Еще можно картинку прикрепить!" rows=10></textarea><br/><br/>
-  <input type=submit class="btn btn-success btn-large" value="Сообщить">
-</form>
-
-@@ post
-ul.thumbnails
-  li.span7
-    .thumbnail.post
-      - if post.image
-        img src="/#{post.image}"
-
-      .caption
-        h5
-          a href="/post/#{post.id}" == post.created_at
-        - if post.text
-          p == post.text
 
   
   
